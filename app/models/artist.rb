cd30db0c20fb14d333f586c20dc6be60f08c7222
DKateryna <derkach.kateryna@gmail.com>
2017-07-03 00:03:47 +0200

    Created edit, update, delete for artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,10 @@
 class Artist < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+
   has_many :songs
 
   def self.order_by_name
