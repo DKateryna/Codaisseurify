ee3bc375e02b83c6f5a6f5016a8ebcb4e5a4698e
DKateryna <derkach.kateryna@gmail.com>
2017-07-02 20:07:36 +0200

    Edit, update and delete songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,9 +12,6 @@ class SongsController < ApplicationController
     end
 
     def create
-      song_params = params.require(:song).permit(:name, :video_url)
-
-      puts "increate"
       @song = Song.new(song_params)
 
       if @song.save!
@@ -22,6 +19,31 @@ class SongsController < ApplicationController
       else
         render "new"
       end
+    end
+
+    def edit
+      @song = Song.find(params[:id])
+    end
+
+    def update
+      @song = Song.find(params[:id])
+
+      if @song.update_attributes(song_params)
+        redirect_to @song
+      else
+        render "edit"
+      end
+    end
+
+    def destroy
+      @song = Song.find(params[:id])
+      @song.destroy
+      redirect_to songs_path
+    end
+
+    private
+    def song_params
+      params.require(:song).permit(:name, :video_url)
     end
 
 
