d87c2fedb8e229f2fcb1ca4250014b4ebe2571d5
DKateryna <derkach.kateryna@gmail.com>
2017-07-09 18:26:54 +0200

    added specs: songs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe Song, type: :model do
       song.valid?
       expect(song.errors).to have_key(:name)
     end
-
-
-  
   end
+
+  describe ".order_by_name" do
+    let(:artist) { create :artist}
+    let!(:song1) { create :song, name: "Litak", artist: artist}
+    let!(:song2) { create :song, name: "Zozulja", artist: artist }
+    let!(:song3) { create :song, name: "Allo", artist: artist }
+
+    it "returns a sorted array of songs by names" do
+      expect(Song.order_by_name).to eq([song3, song1, song2])
+    end
+  end
+
 end
