3107b8e2e75a82f80f6e4eba1d9d9e03336c34bc
DKateryna <derkach.kateryna@gmail.com>
2017-07-15 22:24:25 +0200

    Added API songs show and create

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,36 @@
 class Api::SongsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: {
       songs: Artist.find(params[:artist_id]).songs
     }.to_json
+  end
+
+  def show
+    render status: 200, json: {
+      song: Artist.find(params[:artist_id]).songs.find(params[:id])
+    }.to_json
+  end
+
+  def create
+    song = Song.new(song_params)
+
+    if song.save
+      render status: 201, json: {
+        message: 'Song created',
+        song: Artist.find(params[:artist_id]).songs.find(song.id)
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :audio_url, :artist_id)
   end
 end
