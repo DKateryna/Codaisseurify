9d934564b3e32a7484e3f68e85286489a75b00ac
DKateryna <derkach.kateryna@gmail.com>
2017-07-16 12:43:24 +0200

    Refactored some of the existent functionalities in the show page of the artist

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,3 +1,40 @@
+function onLoadSongsSuccess(data) {
+
+  $("#songs_list").empty();
+  for(var i in data.songs){
+
+      var id = data.songs[i];
+      var songUrl = `/artists/${id.artist_id}/songs/${id.id}`;
+      var href = $('<a>').attr('href', songUrl).html(id.name);
+      var del = $('<a data-confirm="Are you sure?" rel="nofollow" data-method="delete" >[Delete song]</a>').attr('href', songUrl);
+      var li = $('<li>').append(href).append(del);
+
+    $("#songs_list").append(li);
+  }
+}
+
+function loadSongsList(){
+  var artist = $("#song_artist_id").val();
+
+  $.ajax({
+    type: "GET",
+    url: "/api/artists/" + artist + "/songs",
+    contentType: "application/json",
+    dataType: "json",
+  }).done(onLoadSongsSuccess).fail(onAddSongError);
+}
+
+
+function onAddSongSuccess(){
+   $("#song_name").val(null);
+   $("#song_audio_url").val(null);
+   loadSongsList();
+}
+
+function onAddSongError(error){
+  alert(error);
+}
+
 function addSong(event) {
   var name = $("#song_name").val();
   var audioUrl = $("#song_audio_url").val();
@@ -14,10 +51,9 @@ function addSong(event) {
     data: JSON.stringify(newSong),
     contentType: "application/json",
     dataType: "json",
-    success: function(){
-      window.location.reload(true);
-    }
-  });
+    success: onAddSongSuccess,
+
+  }).done(onAddSongSuccess).fail(onAddSongError);
 
 }
 
