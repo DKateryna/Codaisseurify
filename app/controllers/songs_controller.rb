43b3901736e3f89d1bcbfa32e3f81e5cc2ecace1
DKateryna <derkach.kateryna@gmail.com>
2017-10-15 23:29:50 +0200

    fix back to artist from song page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,4 @@
 class SongsController < ApplicationController
-  def index
-    artist = Artist.find(params[:artist_id])
-    @songs = artist.songs
-  end
-
   def show
     artist = Artist.find(params[:artist_id])
     @song = artist.songs.find(params[:id])
